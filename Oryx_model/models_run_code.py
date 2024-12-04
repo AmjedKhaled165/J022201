0b3bc71f8b95ab30df1d3a0b68d6052b6ef2892a
Amjed <122521459+AmjedKhaled165@users.noreply.github.com>
2024-12-04 13:33:03 +0200

    Update models_run_code.py

--- Oryx_model/models_run_code.py
+++ Oryx_model/models_run_code.py
@@ -3,47 +3,47 @@ from ultralytics import YOLO
 import time
 import cv2
 
-output_images_path = "J022201\Oryx_model\phtot for space fall stand"
-name_folder_path = "J022201\Oryx_model\photo for space with stand"
-final_folder_path = "J022201\Oryx_model\photo for product and space"
+phtot_for_space_fall_stand = "J022201\Oryx_model\phtot for space fall stand"
+photo_for_space_with_stand = "J022201\Oryx_model\photo for space with stand"
+photo_for_product_and_space ="J022201\Oryx_model\photo for product and space"
 
-if not os.path.exists(output_images_path):
-    os.makedirs(output_images_path)
-if not os.path.exists(name_folder_path):
-    os.makedirs(name_folder_path)
-if not os.path.exists(final_folder_path):
-    os.makedirs(final_folder_path)
+if not os.path.exists(phtot_for_space_fall_stand):
+    os.makedirs(phtot_for_space_fall_stand)
+if not os.path.exists(photo_for_space_with_stand):
+    os.makedirs(photo_for_space_with_stand)
+if not os.path.exists(photo_for_product_and_space):
+    os.makedirs(photo_for_product_and_space)
 
-for filename in os.listdir(output_images_path):
-    file_path = os.path.join(output_images_path, filename)
+for filename in os.listdir(phtot_for_space_fall_stand):
+    file_path = os.path.join(phtot_for_space_fall_stand, filename)
     if os.path.isfile(file_path):
         os.remove(file_path)
-        print(f"Deleted {filename} from 'test' folder")
+        print(f"Deleted {filename} from 'phtot for space fall stand' folder")
 
-for filename in os.listdir(name_folder_path):
-    file_path = os.path.join(name_folder_path, filename)
+for filename in os.listdir(photo_for_space_with_stand):
+    file_path = os.path.join(photo_for_space_with_stand, filename)
     if os.path.isfile(file_path):
         os.remove(file_path)
-        print(f"Deleted {filename} from 'name' folder")
+        print(f"Deleted {filename} from 'photo for space with stand' folder")
 
-for filename in os.listdir(final_folder_path):
-    file_path = os.path.join(final_folder_path, filename)
+for filename in os.listdir(photo_for_product_and_space):
+    file_path = os.path.join(photo_for_product_and_space, filename)
     if os.path.isfile(file_path):
         os.remove(file_path)
-        print(f"Deleted {filename} from 'final' folder")
+        print(f"Deleted {filename} from 'photo for product and space' folder")
 
-model_test = YOLO("Oryx_01_model.pt")
-model_final = YOLO("yolov8n.pt")
+Oryx_01_empty_space_model = YOLO("Oryx_01_empty_space_model.pt")
+Oryx_01_product_model = YOLO("Oryx_01_product_model.pt")
 
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("Failed to capture frame. Exiting...")
+        print("check num for VideoCapture or cann't open camera")
         break
 
-    results_test = model_test(frame)
+    results_test = Oryx_01_empty_space_model(frame)
 
     if len(results_test[0].boxes) > 0:  
         for box in results_test[0].boxes:
@@ -55,33 +55,32 @@ while True:
 
             cropped_image = frame[y1:y2, :]
             timestamp = int(time.time())
-            test_image_path = os.path.join(output_images_path, f"{class_name}_{timestamp}.jpg")
+            test_image_path = os.path.join(phtot_for_space_fall_stand, f"{class_name}_{timestamp}.jpg")
             cv2.imwrite(test_image_path, cropped_image) 
-            print(f"Test image saved: {test_image_path}")
+            print(f"phtot for space fall stand image saved: {test_image_path}")
 
             resized_image = cv2.resize(cropped_image, (224, 224)) 
-            name_image_path = os.path.join(name_folder_path, f"{class_name}_{timestamp}.jpg")
+            name_image_path = os.path.join(photo_for_space_with_stand, f"{class_name}_{timestamp}.jpg")
             cv2.imwrite(name_image_path, resized_image)
-            print(f"Name image saved: {name_image_path}")
+            print(f"photo for space with stand image saved: {name_image_path}")
 
-    cv2.imshow("Camera", frame)
-
+    cv2.imshow("Oryx", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
 
-for filename in os.listdir(name_folder_path):
-    file_path = os.path.join(name_folder_path, filename)
+for filename in os.listdir(photo_for_space_with_stand):
+    file_path = os.path.join(photo_for_space_with_stand, filename)
     if os.path.isfile(file_path):
-        print(f"Processing {filename} in 'name' folder")
+        print(f"Processing {filename} in 'photo for space with stand' folder")
         frame = cv2.imread(file_path)
         if frame is None:
             print(f"Could not read {filename}, skipping.")
             continue
 
-        results_final = model_final(frame)
+        results_final = Oryx_01_product_model(frame)
 
         if len(results_final[0].boxes) > 0:
             for box in results_final[0].boxes:
@@ -91,6 +90,11 @@ for filename in os.listdir(name_folder_path):
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(frame, class_name, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-            final_image_path = os.path.join(final_folder_path, f"final_{filename}")
+            final_image_path = os.path.join(photo_for_product_and_space, f"photo for product and space_{filename}")
             cv2.imwrite(final_image_path, frame)
-            print(f"Final image saved: {final_image_path}")
+            print(f"photo for product and space image saved: {final_image_path}")
+print("Qryx🛒🛒⛽")
+print("Team👨🏽‍🤝‍👨🏻")
+print("Amjed Khaled , Zyad Ashraf , Mahmoud Bahaa , Salsabil Mohammed , Hanin Ahmed😋😎")
+print("Thank you")
+print("===================================================================================================================================================================================")
