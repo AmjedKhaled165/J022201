f453cc17cb23d2d5a9aa6045026c207c194398ff
Amjed <122521459+AmjedKhaled165@users.noreply.github.com>
2024-12-07 11:58:55 +0200

    Update models_run_code.py

--- Oryx_model/models_run_code.py
+++ Oryx_model/models_run_code.py
@@ -27,14 +27,15 @@ while True:
     results_space = Oryx_01_empty_space_model(frame)
     if len(results_space[0].boxes) > 0:
         for box in results_space[0].boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy[0])
-            class_name = results_space[0].names[int(box.cls[0])]
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(frame, class_name, (x1, y1 + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            if box.conf[0] >= 0.6: 
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                class_name = results_space[0].names[int(box.cls[0])]
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(frame, class_name, (x1, y1 + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
             
-            timestamp = int(time.time())
-            image_path = os.path.join(space_with_stand, f"{class_name}_{timestamp}.jpg")
-            cv2.imwrite(image_path, frame)
+                timestamp = int(time.time())
+                image_path = os.path.join(space_with_stand, f"{class_name}_{timestamp}.jpg")
+                cv2.imwrite(image_path, frame)
 
     cv2.imshow("Oryx", frame)
 
@@ -58,10 +59,11 @@ for filename in os.listdir(space_with_stand):
         results_product = Oryx_01_product_model(frame)
         if len(results_product[0].boxes) > 0:
             for box in results_product[0].boxes:
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                class_name = results_product[0].names[int(box.cls[0])]
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                cv2.putText(frame, class_name, (x1, y1 + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+                if box.conf[0] >= 0.6:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    class_name = results_product[0].names[int(box.cls[0])]
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                    cv2.putText(frame, class_name, (x1, y1 + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
 
             final_product_path = os.path.join(Space_with_product_full, filename)
             cv2.imwrite(final_product_path, frame)
@@ -78,10 +80,11 @@ for filename in os.listdir(space_with_stand):
         results_space = Oryx_01_empty_space_model(frame)
         if len(results_space[0].boxes) > 0:
             for box in results_space[0].boxes:
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                resized_frame = frame[y1-25:y2+25, :]
-                resized_path = os.path.join(space_an_offer_stand, filename)
-                cv2.imwrite(resized_path, resized_frame)
+                if box.conf[0] >= 0.6:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    resized_frame = frame[y1-25:y2+25, :]
+                    resized_path = os.path.join(space_an_offer_stand, filename)
+                    cv2.imwrite(resized_path, resized_frame)
 
 for filename in os.listdir(space_an_offer_stand):
     file_path = os.path.join(space_an_offer_stand, filename)
@@ -93,10 +96,11 @@ for filename in os.listdir(space_an_offer_stand):
         results_product = Oryx_01_product_model(frame)
         if len(results_product[0].boxes) > 0:
             for box in results_product[0].boxes:
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                class_name = results_product[0].names[int(box.cls[0])]
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                cv2.putText(frame, class_name, (x1, y1 + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+                if box.conf[0] >= 0.6:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    class_name = results_product[0].names[int(box.cls[0])]
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                    cv2.putText(frame, class_name, (x1, y1 + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
 
             product_path = os.path.join(product_and_space, filename)
             cv2.imwrite(product_path, frame)
@@ -113,13 +117,14 @@ for filename in os.listdir(product_and_space):
 
         if len(results_space[0].boxes) > 0 and len(results_product[0].boxes) > 0:
             for space_box in results_space[0].boxes:
-                space_x1, space_y1, space_x2, space_y2 = map(int, space_box.xyxy[0])
-                space_width = space_x2 - space_x1 
+                if box.conf[0] >= 0.6:
+                    space_x1, space_y1, space_x2, space_y2 = map(int, space_box.xyxy[0])
+                    space_width = space_x2 - space_x1 
                 
-                for product_box in results_product[0].boxes:
-                    product_x1, product_y1, product_x2, product_y2 = map(int, product_box.xyxy[0])
-                    product_width = product_x2 - product_x1 
+                    for product_box in results_product[0].boxes:
+                        product_x1, product_y1, product_x2, product_y2 = map(int, product_box.xyxy[0])
+                        product_width = product_x2 - product_x1 
 
-                    if product_width < space_width:
-                        final_path = os.path.join(Can_put_Prodect_of_space, filename)
-                        cv2.imwrite(final_path, frame)
+                        if product_width < space_width:
+                            final_path = os.path.join(Can_put_Prodect_of_space, filename)
+                            cv2.imwrite(final_path, frame)
